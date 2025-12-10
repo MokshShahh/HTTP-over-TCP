7d603efc7c1c84e4fbca9d43a450a412880d98ce
Moksh <moksh13112006@gmail.com>
2025-12-10 23:04:04 +0530

    data printed line by line

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
@@ -11,12 +12,21 @@ func main() {
 	if err != nil {
 		log.Fatal("error", err)
 	}
+	line := ""
 	for {
 		data := make([]byte, 8)
 		n, err := file.Read(data)
+		data = data[:n]
+		fmt.Print(data)
+		if i := bytes.IndexByte(data, '\n'); i != -1 {
+			line += string(data[:i])
+			data = data[i+1:]
+			fmt.Printf("read: %s\n", line)
+			line = ""
+		}
+		line += string(data)
 		if err != nil {
 			break
 		}
-		fmt.Printf("read: %s\n", string(data[:n]))
 	}
 }
