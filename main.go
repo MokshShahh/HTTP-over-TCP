356b4a6fead4e00440bcae0916e9db3a7b28fc8a
Moksh <moksh13112006@gmail.com>
2025-12-12 00:30:42 +0530

    fix: uses go routine + channel

--- main.go
+++ main.go
@@ -3,30 +3,47 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+	go func() {
+		defer f.Close()
+		defer close(out)
+		line := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				line += string(data[:i])
+				data = data[i+1:]
+				out <- line
+				line = ""
+			}
+			line += string(data)
+			if err != nil {
+				break
+			}
+		}
+
+	}()
+
+	return out
+
+}
 
 func main() {
 	file, err := os.Open("messages.txt")
 	if err != nil {
 		log.Fatal("error", err)
 	}
-	line := ""
-	for {
-		data := make([]byte, 8)
-		n, err := file.Read(data)
-		data = data[:n]
-		fmt.Print(data)
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			line += string(data[:i])
-			data = data[i+1:]
-			fmt.Printf("read: %s\n", line)
-			line = ""
-		}
-		line += string(data)
-		if err != nil {
-			break
-		}
+	lines := getLinesChannel(file)
+	for line := range lines {
+		fmt.Printf("Read: %s\n", line)
+
 	}
 }
