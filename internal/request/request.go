21e339ad8b2e814d435bacc6a17d42785b6749b3
Moksh <moksh13112006@gmail.com>
2025-12-18 01:08:19 +0530

    feat: requestline parsing

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,6 +1,11 @@
 package request
 
-import "io"
+import (
+	"fmt"
+	"io"
+	"strings"
+	"unicode"
+)
 
 type Request struct {
 	RequestLine RequestLine
@@ -13,5 +18,30 @@ type RequestLine struct {
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+	lines := strings.Split(string(data), "\r\n")
+	requestLine := strings.Fields(lines[0])
+	method := requestLine[0]
+	for _, r := range method {
+		if unicode.IsLetter(r) && !unicode.IsUpper(r) {
+			return nil, fmt.Errorf("method is not in all uppercase")
+		}
+		if !unicode.IsLetter(r) {
+			return nil, fmt.Errorf("method is not alphabetical")
+		}
+	}
+	httpVer := strings.TrimSpace(requestLine[2])
+	if httpVer != "HTTP/1.1" {
+		return nil, fmt.Errorf("incorrect http ver: expected HTTP/1.1, got %s", httpVer)
+	}
+	return &Request{
+		RequestLine: RequestLine{
+			Method:        requestLine[0],
+			RequestTarget: requestLine[1],
+			HttpVersion:   "1.1",
+		},
+	}, nil
 }
