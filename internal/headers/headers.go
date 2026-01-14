19e37b998f2e0a3569201e661e7b59d5f922d74a
Moksh <moksh13112006@gmail.com>
2026-01-14 23:44:10 +0530

    headers parsing w err handling

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -1,7 +1,55 @@
 package headers
 
+import (
+	"bytes"
+	"fmt"
+)
+
 type Headers map[string]string
 
+var crlf = []byte("\r\n")
+
+func NewHeaders() Headers {
+	return map[string]string{}
+}
+
+func parseHeader(fieldLine []byte) (string, string, error) {
+	parts := bytes.SplitN(fieldLine, []byte(":"), 2)
+	if len(parts) != 2 {
+		fmt.Print("hi")
+		fmt.Printf("%d", len(parts))
+		return "", "", fmt.Errorf("Header is not in correct form")
+	}
+	name := parts[0]
+	val := bytes.TrimSpace(parts[1])
+
+	//fieldname cannot have whitespace before semicolon
+	if bytes.HasSuffix(name, []byte(" ")) {
+		return "", "", fmt.Errorf("fieldname has whitespace")
+	}
+	return string(name), string(val), nil
+}
+
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	read := 0
+	done = false
+	for {
+		idx := bytes.Index(data[read:], crlf)
+		if idx == -1 {
+			break
+		}
+		//empty header
+		if idx == 0 {
+			done = true
+			read += len(crlf)
+		}
+		name, val, err := parseHeader(data[read : read+idx])
+		if err != nil {
+			return 0, false, err
+		}
+		read += idx + len(crlf)
+		h[name] = val
+	}
+	return read, done, nil
 
 }
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -10,7 +10,7 @@ import (
 func TestParseHeaders(t *testing.T) {
 	// Test: Valid single header
 	headers := NewHeaders()
-	data := []byte("Host: localhost:42069\r\n\r\n")
+	data := []byte("Host: localhost:42069\r\n")
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
