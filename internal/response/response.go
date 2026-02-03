ad1d28b268ef2affab8d64eb111f3d7afcd3ad3c
Moksh <moksh13112006@gmail.com>
2026-02-03 22:33:17 +0530

    feat(response): refactored funcs

--- internal/response/response.go
+++ internal/response/response.go
@@ -19,7 +19,31 @@ const (
 	StatusServerErr StatusCode = 500
 )
 
-func WriteStatusLine(w io.Writer, statusCode StatusCode) error {
+type writerState int
+
+const (
+	stateStatus writerState = iota
+	stateHeaders
+	stateBody
+	stateDone
+)
+
+type Writer struct {
+	w     io.Writer
+	state writerState
+}
+
+func NewWriter(w io.Writer) *Writer {
+	return &Writer{
+		w:     w,
+		state: stateStatus,
+	}
+}
+
+func (w *Writer) WriteStatusLine(statusCode StatusCode) error {
+	if w.state != stateStatus {
+		return fmt.Errorf("not in status line state")
+	}
 	statusLine := []byte{}
 	switch statusCode {
 	case StatusOK:
@@ -31,8 +55,37 @@ func WriteStatusLine(w io.Writer, statusCode StatusCode) error {
 	default:
 		return fmt.Errorf("unrecognised code")
 	}
-	_, err := w.Write(statusLine)
+	_, err := w.w.Write(statusLine)
+	w.state = stateHeaders
 	return err
+
+}
+
+func (w *Writer) WriteHeaders(headers headers.Headers) error {
+	if w.state != stateHeaders {
+		return fmt.Errorf("state is not header state")
+	}
+	for key, value := range headers {
+		_, err := w.w.Write([]byte(fmt.Sprintf("%s: %s\r\n", key, value)))
+		if err != nil {
+			return err
+		}
+	}
+	_, err := w.w.Write([]byte("\r\n"))
+	w.state = stateBody
+	return err
+}
+func (w *Writer) WriteBody(p []byte) (int, error) {
+	if w.state != stateBody {
+		return 0, fmt.Errorf("state is not state body")
+	}
+	n, err := w.w.Write(p)
+	if err != nil {
+		return 0, err
+	}
+	w.state = stateDone
+	return n, nil
+
 }
 
 func GetDefaultHeaders(contentLen int) headers.Headers {
@@ -42,15 +95,4 @@ func GetDefaultHeaders(contentLen int) headers.Headers {
 	h["content-type"] = "text/plain"
 	return h
 
-}
-
-func WriteHeaders(w io.Writer, headers headers.Headers) error {
-	for key, value := range headers {
-		_, err := w.Write([]byte(fmt.Sprintf("%s: %s\r\n", key, value)))
-		if err != nil {
-			return err
-		}
-	}
-	_, err := w.Write([]byte("\r\n"))
-	return err
 }
